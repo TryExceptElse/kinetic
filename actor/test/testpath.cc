5947dfc5e54fe2438ed48f8839715d0efc6d2d97
tryexceptelse <schwarzjohnv@gmail.com>
2018-05-08 18:16:33 -0700

    Added Maneuver::FindThrustVector() tests.

--- actor/test/testpath.cc
+++ actor/test/testpath.cc
@@ -79,3 +79,105 @@ TEST_CASE( "test segment group can predict half orbit", "[BallisticSegment]" ) {
     REQUIRE( result.r.y == Approx(365151451881.22858).epsilon(0.0001) );
     REQUIRE( result.r.z == Approx(14442203602.998617).epsilon(0.0001) );
 }
+
+TEST_CASE( "test prograde thrust vector is calculated", "[Maneuver]" ) {
+    const kin::Body ref(kin::G * 5.972e24, 6371000.0);
+    const kin::Vector r(50.0, -50.0, 2);
+    const kin::Vector v(-0.7, -0.7, 0.0);
+    const double t = 0.0;
+    const kin::PerformanceData performance(3500, 10000);
+    const kin::Maneuver maneuver(kin::kPrograde, 100.0, performance, 80, 0.0);
+
+    const kin::Vector result = maneuver.FindThrustVector(ref, r, v, 0.0);
+
+    const kin::Vector expected(-0.70710678118, -0.70710678118, 0);
+
+    REQUIRE( result.x == Approx(-0.70710678118  ).epsilon(0.0001) );
+    REQUIRE( result.y == Approx(-0.70710678118  ).epsilon(0.0001) );
+    REQUIRE( result.z == Approx(0.0             ).epsilon(0.0001) );
+}
+
+TEST_CASE( "test retrograde thrust vector is calculated", "[Maneuver]" ) {
+    const kin::Body ref(kin::G * 5.972e24, 6371000.0);
+    const kin::Vector r(50.0, -50.0, 2);
+    const kin::Vector v(-0.7, -0.7, 0.0);
+    const double t = 0.0;
+    const kin::PerformanceData performance(3500, 10000);
+    const kin::Maneuver maneuver(kin::kRetrograde, 100.0, performance, 80, 0.0);
+
+    const kin::Vector result = maneuver.FindThrustVector(ref, r, v, 0.0);
+
+    const kin::Vector expected(-0.70710678118, -0.70710678118, 0);
+
+    REQUIRE( result.x == Approx(0.70710678118   ).epsilon(0.0001) );
+    REQUIRE( result.y == Approx(0.70710678118   ).epsilon(0.0001) );
+    REQUIRE( result.z == Approx(0.0             ).epsilon(0.0001) );
+}
+
+TEST_CASE( "test radial thrust vector is calculated", "[Maneuver]" ) {
+    const kin::Body ref(kin::G * 5.972e24, 6371000.0);
+    const kin::Vector r(50.0, -50.0, 0.0);
+    const kin::Vector v(-0.7, -0.7, 3.0);
+    const double t = 0.0;
+    const kin::PerformanceData performance(3500, 10000);
+    const kin::Maneuver maneuver(kin::kRadial, 100.0, performance, 80, 0.0);
+
+    const kin::Vector result = maneuver.FindThrustVector(ref, r, v, 0.0);
+
+    const kin::Vector expected(-0.70710678118, -0.70710678118, 0);
+
+    REQUIRE( result.x == Approx(0.70710678118   ).epsilon(0.0001) );
+    REQUIRE( result.y == Approx(-0.70710678118  ).epsilon(0.0001) );
+    REQUIRE( result.z == Approx(0.0             ).epsilon(0.0001) );
+}
+
+TEST_CASE( "test anti-radial thrust vector is calculated", "[Maneuver]" ) {
+    const kin::Body ref(kin::G * 5.972e24, 6371000.0);
+    const kin::Vector r(50.0, -50.0, 0.0);
+    const kin::Vector v(-0.7, -0.7, 0.0);
+    const double t = 0.0;
+    const kin::PerformanceData performance(3500, 10000);
+    const kin::Maneuver maneuver(kin::kAntiRadial, 100.0, performance, 80, 0.0);
+
+    const kin::Vector result = maneuver.FindThrustVector(ref, r, v, 0.0);
+
+    const kin::Vector expected(-0.70710678118, -0.70710678118, 0);
+
+    REQUIRE( result.x == Approx(-0.70710678118  ).epsilon(0.0001) );
+    REQUIRE( result.y == Approx(0.70710678118   ).epsilon(0.0001) );
+    REQUIRE( result.z == Approx(0.0             ).epsilon(0.0001) );
+}
+
+TEST_CASE( "test normal thrust vector is calculated", "[Maneuver]" ) {
+    const kin::Body ref(kin::G * 5.972e24, 6371000.0);
+    const kin::Vector r(50.0, -50.0, 2.0);
+    const kin::Vector v(-0.7, -0.7, 0.0);
+    const double t = 0.0;
+    const kin::PerformanceData performance(3500, 10000);
+    const kin::Maneuver maneuver(kin::kNormal, 100.0, performance, 80, 0.0);
+
+    const kin::Vector result = maneuver.FindThrustVector(ref, r, v, 0.0);
+
+    const kin::Vector expected(-0.70710678118, -0.70710678118, 0);
+
+    REQUIRE( result.x == Approx(0.019992   ).epsilon(0.0001) );
+    REQUIRE( result.y == Approx(-0.019992    ).epsilon(0.0001) );
+    REQUIRE( result.z == Approx(-0.9996      ).epsilon(0.0001) );
+}
+
+TEST_CASE( "test anti-normal thrust vector is calculated", "[Maneuver]" ) {
+    const kin::Body ref(kin::G * 5.972e24, 6371000.0);
+    const kin::Vector r(50.0, -50.0, 2.0);
+    const kin::Vector v(-0.7, -0.7, 0.0);
+    const double t = 0.0;
+    const kin::PerformanceData performance(3500, 10000);
+    const kin::Maneuver maneuver(kin::kAntiNormal, 100.0, performance, 80, 0.0);
+
+    const kin::Vector result = maneuver.FindThrustVector(ref, r, v, 0.0);
+
+    const kin::Vector expected(-0.70710678118, -0.70710678118, 0);
+
+    REQUIRE( result.x == Approx(-0.019992    ).epsilon(0.0001) );
+    REQUIRE( result.y == Approx(0.019992   ).epsilon(0.0001) );
+    REQUIRE( result.z == Approx(0.9996     ).epsilon(0.0001) );
+}
