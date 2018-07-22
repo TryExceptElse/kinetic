3c4c4643b7356a021e50cdeb008b34b46d2e2348
tryexceptelse <schwarzjohnv@gmail.com>
2018-07-21 21:02:02 -0700

    Updated vector .pyi

--- server/server/model/vector.pyi
+++ server/server/model/vector.pyi
@@ -3,8 +3,8 @@ class PyVector:
     """ Wraps Vector """
     def __init__(self, x=0, y=0, z=0): ...
 
-    sqlen = ...  # type: float
-    len = ...  # type: float
+    squared_norm = ...  # type: float
+    norm = ...  # type: float
     x = ...  # type: float
     y = ...  # type: float
     z = ...  # type: float
