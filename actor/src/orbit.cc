93a84f85c50ff04eebc69461752c2072e265e0c3
tryexceptelse <schwarzjohnv@gmail.com>
2018-07-25 21:56:21 -0700

    Added more error checking to orbit methods.

--- actor/src/orbit.cc
+++ actor/src/orbit.cc
@@ -47,6 +47,10 @@ double Orbit::period() const {
 }
 
 double Orbit::mean_motion() const {
+    if (e >= 1.0) {
+        throw std::runtime_error(
+            "Orbit::mean_motion() : Eccentricity >= 1: " + std::string(e))
+    }
     return std::sqrt(u / (a*a*a));
 }
 
@@ -67,7 +71,19 @@ double Orbit::max_speed() const {
 }
 
 double Orbit::eccentric_anomaly() const {
-    double E = acos((e + cos(t)) / (1 + e * cos(t)));
+    double E;
+    if (0.0 < e && e < 1.0) {
+        E = acos((e + cos(t)) / (1 + e * cos(t)));
+    } else if (e > 1.0) {
+        E = std::acosh((e + std::cos(t)) / (1.0 + e * std::cos(t)));
+    } else if (e == 1.0) {
+        E = std::tan(t / 2.0);
+    } else if (e == 0.0) {
+        E = t;
+    } else {
+        throw std::runtime_error("Orbit::eccentric_anomaly() : "
+            "Invalid e: " + std::to_string(e));
+    }
     if (t > PI && E < PI) {
         E = 2*PI - E;
     }
@@ -75,6 +91,7 @@ double Orbit::eccentric_anomaly() const {
 }
 
 double Orbit::mean_anomaly() const {
+    // TODO: Should work regardless of eccentricity
     double E = eccentric_anomaly();
     double M = E - e * sin(E);
     if (E > PI && M < PI) {
@@ -88,9 +105,27 @@ double Orbit::mean_anomaly() const {
  */
 Vector Orbit::position() const {
     const double ea = eccentric_anomaly();
+    double p, q;
     // Generate positions in orbital plane.
-    const double p = a * (std::cos(ea) - e);
-    const double q = a * std::sin(ea) * std::sqrt(1 - std::pow(e, 2));
+    if (0.0 < e && e < 1.0) {
+        p = a * (std::cos(ea) - e);
+        q = a * std::sin(ea) * std::sqrt(1 - std::pow(e, 2));
+    } else {
+        double r;
+        if (e > 1.0) {
+            const double semi_latus = a * (1.0 - e * e);
+            r = semi_latus / (1.0 + e * cos(t));
+        } else if (e == 1.0) {
+            // TODO
+        } else if (e == 0.0) {
+            r = a;
+        } else {
+            throw std::runtime_error("Orbit::position() : "
+                "Invalid e: " + std::to_string(e));
+        }
+        p = std::cos(t) * r;
+        q = std::sin(t) * r;
+    }
     Vector untransformed_position(p, q, 0);
 
     // Transform position
@@ -172,6 +207,9 @@ void Orbit::CalcFromPosVel(const Vector r, const Vector v) {
  * https://en.wikipedia.org/wiki/True_anomaly
  */
 void Orbit::CalcTrueAnomaly(const double eccentric_anomaly) {
+    if (std::isnan(eccentric_anomaly)) {
+        throw std::invalid_argument("Orbit::CalcTrueAnomaly passed NaN.");
+    }
     const double E = eccentric_anomaly;
     this->t = std::acos((std::cos(E) - e) / (1 - e * std::cos(E)));
     if (eccentric_anomaly > PI && this->t < PI) {
@@ -180,11 +218,17 @@ void Orbit::CalcTrueAnomaly(const double eccentric_anomaly) {
 }
 
 double Orbit::EstimateTrueAnomaly(const double mean_anomaly) const {
+    if (std::isnan(mean_anomaly)) {
+        throw std::invalid_argument("Orbit::EstimateTrueAnomaly passed NaN.");
+    }
     const double M = mean_anomaly;
     return M + 2 * e * std::sin(M) + 1.25 * e * e * std::sin(2 * M);
 }
 
 double Orbit::CalcEccentricAnomaly(const double mean_anomaly) const {
+    if (std::isnan(mean_anomaly)) {
+        throw std::invalid_argument("Orbit::CalcEccentricAnomaly passed NaN.");
+    }
     const double t = EstimateTrueAnomaly(mean_anomaly);
     double E = acos((e + cos(t)) / (1 + e * cos(t)));
     double M = E - e * sin(E);
